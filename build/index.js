01f4368c1490f6f9168cf67d957eeca5598912f8
JowayYoung <young.joway@aliyun.com>
2020-01-28 16:58:52 +0800

    优化打包机制

--- build/index.js
+++ build/index.js
@@ -7,10 +7,12 @@ import { BuildCb } from "./tool";
 
 (async() => {
 	const rimraf = Util.promisify(Rimfaf);
+	const commonUmd = WebpackConfig("common");
 	const browserUmd = WebpackConfig("browser");
 	const nodeUmd = WebpackConfig("node");
 	await rimraf("dist");
 	await rimraf("test/dist");
+	await BuildCb(commonUmd);
 	await BuildCb(browserUmd);
 	await BuildCb(nodeUmd);
 	await RecursiveCopy("dist", "test/dist");
