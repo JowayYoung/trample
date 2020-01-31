ce8e8b7cb0279fa8cc333907c99e5a7ebceca7ac
JowayYoung <young.joway@aliyun.com>
2020-01-31 21:35:08 +0800

    update

--- test/web/src/templates/helpers/title-case.js
+++
@@ -1,7 +0,0 @@
-export default function(title) {
-	// console.log(this.page);
-	return title.split("-").reduce((t, c) => {
-		const item = c.toLowerCase().replace(/( |^)[a-z]/g, w => w.toUpperCase());
-		return t + item;
-	}, "");
-};
