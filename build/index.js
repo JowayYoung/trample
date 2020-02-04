1118afd795d7d7c8861cd62f434e5ec3fdeba675
JowayYoung <young.joway@aliyun.com>
2020-02-04 20:49:33 +0800

    完善文件复制功能

--- build/index.js
+++ build/index.js
@@ -2,32 +2,26 @@ import Util from "util";
 import Rimfaf from "rimraf";
 import CopyDir from "copy-dir";
 
-import { BuildCb, WebpackConfig } from "./util";
+import { AbsPath, BuildCb, WebpackConfig } from "./util";
 
 (async() => {
 	const rimraf = Util.promisify(Rimfaf);
-	const webUmd = WebpackConfig("web");
-	const webUmdEs5 = WebpackConfig("web", true);
 	const nodeUmd = WebpackConfig("node");
 	const nodeUmdEs5 = WebpackConfig("node", true);
-	const delList = ["web.js", "web.es5.js", "node.js", "node.es5.js", "common", "web", "node"];
-	const buildList = [webUmd, webUmdEs5, nodeUmd, nodeUmdEs5];
-	const filterList = ["web.js", "node.js"];
+	const webUmd = WebpackConfig("web");
+	const webUmdEs5 = WebpackConfig("web", true);
+	const delList = ["common", "node", "web", "node.js", "node.es5.js", "web.js", "web.es5.js"];
+	const buildList = [nodeUmd, nodeUmdEs5, webUmd, webUmdEs5];
+	const filterList = [".DS_Store", "node.js", "web.js"];
 	for (const v of delList) {
 		await rimraf(v);
 	}
 	for (const v of buildList) {
 		await BuildCb(v);
 	}
-	CopyDir.sync("src", "", {
+	CopyDir.sync(AbsPath("../src"), AbsPath(".."), {
 		cover: true,
-		filter(stat, filepath, filename) {
-			console.log(stat, filepath, filename);
-			if (stat === "file" && filterList.includes(filename)) {
-				return false;
-			}
-			return true;
-		},
+		filter: (stat, filepath) => !(stat === "file" && filterList.some(v => filepath.includes(v))),
 		mode: true,
 		utimes: true
 	});
